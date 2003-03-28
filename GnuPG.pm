82b6b06ab9c3ef5fe514813c9efd06ebe3d426ec
Robert Spier <rspier@cpan.org>
2003-03-28 04:38:08 +0000

    Import of RSPIER/Mail-GnuPG-0.02 from CPAN.
    gitpan-cpan-distribution: Mail-GnuPG
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         RSPIER/Mail-GnuPG-0.02.tar.gz
    gitpan-cpan-author:       RSPIER
    gitpan-cpan-maturity:     released

--- GnuPG.pm
+++ GnuPG.pm
@@ -21,7 +21,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use GnuPG::Interface;
 use IO::Handle;
@@ -582,6 +582,11 @@ GNU General Public License or the Artistic License for more details.
 =head1 AUTHOR
 
 Robert Spier
+
+=head1 BUGS/ISSUES/PATCHES
+
+Please send all bugs/issues/patches to
+    bug-Mail-GnuPG@rt.cpan.org
 
 =head1 SEE ALSO
 
